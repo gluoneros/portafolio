4ca02e2e30aa5be615b5dda1155db962589e2888
gluoneros <gluoneros@gmail.com>
2023-12-20 23:35:36 -0500

    commit 20/12 avance

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render, HttpResponse
 
-
-
 # Create your views here.
 def home(request):
     return render(request, "core/home.html")
@@ -9,8 +7,6 @@ def home(request):
 def about(request):
     return render(request, "core/about.html")
 
-def portafolio(request):
-    return render(request, "core/portafolio.html")
+def contact(request):
+    return render(request, "core/contact.html")
 
-def contacto(request):
-    return render(request, "core/contacto.html")
